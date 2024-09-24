7586ed324437b5432885fe1a2f5eee4cc6bf4818
Shutov Dmitrii <fplatoff@gmail.com>
2024-09-24 18:42:25 +0400

    tilda-style animation

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-document.addEventListener('mousemove', function(e) {
+document.addEventListener('mousemove', (e) => cursorTrail(e), false);
+document.addEventListener('mouseenter', (e) => cursorTrail(e), false);
+
+function cursorTrail(e) {
+    const DIF = 0
     let trail = document.createElement('div');
     trail.className = 'trail-dot';
-    trail.style.left = `${e.pageX}px`;  // TODO: make in slytly random place 
-    trail.style.top = `${e.pageY}px`;
+    const  dispers = () =>  DIF - Math.random() * DIF;
+    trail.style.left = `${e.pageX + dispers()}px`;  
+    trail.style.top = `${e.pageY + dispers()}px`;
     document.body.appendChild(trail);
+    let trails = document.querySelectorAll('.trail-dot');    
 
     setTimeout(() => {
-        trail.style.transform = 'scale(0)';
-        trail.style.opacity = '0';
-    }, 30);
+        // trails.forEach(el => el.style.transform = 'scale(0)');
+        trail.style.transform = 'scale(0)'
+        // trails.forEach(el => el.style.opacity -= 0.1);
+        // trails.forEach(el.style.opacity < 0.1 ? el.remove() : null);
+    }, 50);
 
     setTimeout(() => {
         trail.remove();
-    }, 100);
-});
+    }, 200);
+};
